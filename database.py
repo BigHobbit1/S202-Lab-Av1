3c56e54e3cf24a75e15de21128600f83d6f87e26
Vitor Pestalozi <126729075+BigHobbit1@users.noreply.github.com>
2025-04-10 16:15:51 -0300

    feat: Correções

--- database.py
+++ database.py
@@ -14,21 +14,14 @@ class Database:
             )
             self.db = self.cluster_connection[database]
             self.collection = self.db[collection]
-            print("✅ Conectado ao banco de dados com sucesso!")
+            print("Conectado ao banco de dados com sucesso!")
         except Exception as e:
-            print(f"❌ Erro ao conectar: {e}")
+            print(e)
 
     def reset_database(self, dataset: list):
-        """
-        Reseta a coleção atual do banco de dados, removendo todos os documentos e populando com o dataset fornecido.
-
-        :param dataset: Lista de documentos (dicionários) para inserir na coleção após o reset.
-        """
         try:
-            # Remove todos os documentos da coleção atual
             self.collection.drop()
 
-            # Insere o novo dataset
             if dataset:
                 self.collection.insert_many(dataset)
                 print("Novo dataset inserido com sucesso!")
@@ -36,4 +29,4 @@ class Database:
                 print("Nenhum dado para inserir na coleção.")
 
         except Exception as e:
-            print(f"❌ Erro ao resetar o banco de dados: {e}")
+            print(e)
--- main.py
+++ main.py
@@ -2,17 +2,11 @@ from database import Database
 from motoristaDAO import MotoristaDAO
 from motoristaCLI import MotoristaCLI
 
-def main():
-    # Configuração do banco de dados
-    print("Iniciando a conexão com o banco de dados...")
-    db = Database("Avaliacao","Motoristas")
-    motorista_dao = MotoristaDAO(db)
-
-    # Inicializando o CLI
-    motoristaCLI = MotoristaCLI(motorista_dao)
-    print("Bem-vindo ao sistema de gerenciamento de motoristas!")
-    print("Comandos disponíveis: create, read, update, delete, quit")
-    motoristaCLI.run()
-
-if __name__ == "__main__":
-    main()
+print("Iniciando a conexão com o banco de dados...")
+db = Database("Avaliacao","Motoristas")
+motorista_dao = MotoristaDAO(db)
+  
+motoristaCLI = MotoristaCLI(motorista_dao)
+print("Bem-vindo ao sistema de gerenciamento de motoristas!")
+print("Comandos disponíveis: create, read, update, delete, quit")
+motoristaCLI.run()
